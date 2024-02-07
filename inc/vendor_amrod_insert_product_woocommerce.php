e4a87c554ec2c656a0b111ffe87be95c1928d2d5
raisul islam <mdraju3248@gmial.com>
2024-02-07 20:00:16 +0600

    retrieve stock info from stock variable

--- inc/vendor_amrod_insert_product_woocommerce.php
+++ inc/vendor_amrod_insert_product_woocommerce.php
@@ -1,10 +1,12 @@
 <?php
 // Include necessary files
 require_once VENDOR_PLUGIN_PATH . '/vendor/autoload.php';
+
 use Automattic\WooCommerce\Client;
 
 // Function to insert products into WooCommerce
-function product_insert_woocommerce() {
+function product_insert_woocommerce()
+{
 
     // Get global $wpdb object
     global $wpdb;
@@ -19,30 +21,30 @@ function product_insert_woocommerce() {
     $branding_price_table_name = $wpdb->prefix . 'sync_branding_price';
 
     // Retrieve pending products from the database
-    $products = $wpdb->get_results( "SELECT * FROM $product_table_name WHERE status = 'pending' LIMIT 1" );
-    $stocks = $wpdb->get_results( "SELECT * FROM $stock_table_name  LIMIT 1" );
-    $category = $wpdb->get_results( "SELECT * FROM $category_table_name  LIMIT 1" );
-    $brand = $wpdb->get_results( "SELECT * FROM $brand_table_name  LIMIT 1" );
-    $price = $wpdb->get_results( "SELECT * FROM $price_table_name  LIMIT 1" );
-    $branding_db = $wpdb->get_results( "SELECT * FROM $branding_dp_table_name  LIMIT 1" );
-    $branding_price = $wpdb->get_results( "SELECT * FROM $branding_price_table_name  LIMIT 1" );
+    $products = $wpdb->get_results("SELECT * FROM $product_table_name WHERE status = 'pending' LIMIT 1");
+    $stocks = $wpdb->get_results("SELECT * FROM $stock_table_name  LIMIT 1");
+    $category = $wpdb->get_results("SELECT * FROM $category_table_name  LIMIT 1");
+    $brand = $wpdb->get_results("SELECT * FROM $brand_table_name  LIMIT 1");
+    $price = $wpdb->get_results("SELECT * FROM $price_table_name  LIMIT 1");
+    $branding_db = $wpdb->get_results("SELECT * FROM $branding_dp_table_name  LIMIT 1");
+    $branding_price = $wpdb->get_results("SELECT * FROM $branding_price_table_name  LIMIT 1");
 
     // WooCommerce store information
     $website_url     = home_url();
     $consumer_key    = 'ck_43fc16f5ebb0dfdde9bc2d9d5abd7615170d5b3e';
     $consumer_secret = 'cs_2f85757eec5b1c7b482855005351e0c47bca9dcb';
 
-    foreach ( $products as $product ) {
-        
+    foreach ($products as $product) {
+
         $product_data = $product->operation_value;
-        
+
         $product_data = json_decode($product_data);
         // echo "<pre>";
         // print_r($product_data);
         // echo "</pre>";
         // die();
         // Retrieve product data
-        
+
         $product_name = $product_data->productName;
         $description = $product_data->description;
         $inventory = $product_data->inventoryType;
@@ -54,7 +56,16 @@ function product_insert_woocommerce() {
         $minimum = $product_data->minimum;
         $maximum = $product_data->maximum;
 
-        
+
+        foreach ($stocks as $stock) {
+            $stock_data = $stock->operation_value;
+            $stock_data = json_decode($stock_data);
+
+            $simpleCode_stock = $stock_data->simpleCode;
+            $fullCode_stock = $stock_data->fullCode;
+            $stock_stock = $stock_data->stock;
+        }
+
 
         // Set up the API client with WooCommerce store URL and credentials
         $client = new Client(
@@ -79,9 +90,9 @@ function product_insert_woocommerce() {
         );
 
         // Check if the product already exists
-        $existing_products = new WP_Query( $args );
+        $existing_products = new WP_Query($args);
 
-        if ( $existing_products->have_posts() ) {
+        if ($existing_products->have_posts()) {
             $existing_products->the_post();
 
             // get product id
@@ -103,8 +114,7 @@ function product_insert_woocommerce() {
             ];
 
             // update product
-            $client->put( 'products/' . $product_id, $product_data );
-
+            $client->put('products/' . $product_id, $product_data);
         } else {
 
             // Create a new product if not exists
@@ -123,70 +133,69 @@ function product_insert_woocommerce() {
             ];
 
             // Create the product
-            $product    = $client->post( 'products', $product_data );
+            $product    = $client->post('products', $product_data);
             $product_id = $product->id;
 
             // Set product information
-            wp_set_object_terms( $product_id, 'simple', 'product_type' );
-            update_post_meta( $product_id, '_visibility', 'visible' );
-            update_post_meta( $product_id, '_stock_status', 'instock' );
-            update_post_meta( $product_id, '_regular_price', $regular_price );
-            update_post_meta( $product_id, '_sale_price', $sale_price );
-            update_post_meta( $product_id, '_price', $sale_price );
-            update_post_meta( $product_id, '_bigbuy-testimonial', $testimonial );
-            update_post_meta( $product_id, '_claim_1', $claim_1 );
-            update_post_meta( $product_id, '_benefices', $benefices );
-            update_post_meta( $product_id, '_seo_title', $seo_title );
+            wp_set_object_terms($product_id, 'simple', 'product_type');
+            update_post_meta($product_id, '_visibility', 'visible');
+            update_post_meta($product_id, '_stock_status', 'instock');
+            update_post_meta($product_id, '_regular_price', $regular_price);
+            update_post_meta($product_id, '_sale_price', $sale_price);
+            update_post_meta($product_id, '_price', $sale_price);
+            update_post_meta($product_id, '_bigbuy-testimonial', $testimonial);
+            update_post_meta($product_id, '_claim_1', $claim_1);
+            update_post_meta($product_id, '_benefices', $benefices);
+            update_post_meta($product_id, '_seo_title', $seo_title);
 
             // Update product meta data in WordPress
-            update_post_meta( $product_id, '_stock', $stock );
+            update_post_meta($product_id, '_stock', $stock);
 
             // display out of stock message if stock is 0
-            if ( $stock <= 0 ) {
-                update_post_meta( $product_id, '_stock_status', 'outofstock' );
+            if ($stock <= 0) {
+                update_post_meta($product_id, '_stock_status', 'outofstock');
             } else {
-                update_post_meta( $product_id, '_stock_status', 'instock' );
+                update_post_meta($product_id, '_stock_status', 'instock');
             }
-            update_post_meta( $product_id, '_manage_stock', 'yes' );
+            update_post_meta($product_id, '_manage_stock', 'yes');
 
 
             // set product gallery images
-            foreach ( $image_url_array as $image_url ) {
+            foreach ($image_url_array as $image_url) {
 
                 // Extract image name
-                $image_name = basename( $image_url );
+                $image_name = basename($image_url);
                 // Get WordPress upload directory
                 $upload_dir = wp_upload_dir();
 
                 // Download the image from URL and save it to the upload directory
-                $image_data = file_get_contents( $image_url );
+                $image_data = file_get_contents($image_url);
 
-                if ( $image_data !== false ) {
+                if ($image_data !== false) {
                     $image_file = $upload_dir['path'] . '/' . $image_name;
-                    file_put_contents( $image_file, $image_data );
+                    file_put_contents($image_file, $image_data);
 
                     // Prepare image data to be attached to the product
                     $file_path = $upload_dir['path'] . '/' . $image_name;
-                    $file_name = basename( $file_path );
+                    $file_name = basename($file_path);
 
                     // Insert the image as an attachment
                     $attachment = [
-                        'post_mime_type' => mime_content_type( $file_path ),
-                        'post_title'     => preg_replace( '/\.[^.]+$/', '', $file_name ),
+                        'post_mime_type' => mime_content_type($file_path),
+                        'post_title'     => preg_replace('/\.[^.]+$/', '', $file_name),
                         'post_content'   => '',
                         'post_status'    => 'inherit',
                     ];
 
-                    $attach_id = wp_insert_attachment( $attachment, $file_path, $product_id );
+                    $attach_id = wp_insert_attachment($attachment, $file_path, $product_id);
 
                     // Add the image to the product gallery
-                    $gallery_ids   = get_post_meta( $product_id, '_product_image_gallery', true );
-                    $gallery_ids   = explode( ',', $gallery_ids );
+                    $gallery_ids   = get_post_meta($product_id, '_product_image_gallery', true);
+                    $gallery_ids   = explode(',', $gallery_ids);
                     $gallery_ids[] = $attach_id;
-                    update_post_meta( $product_id, '_product_image_gallery', implode( ',', $gallery_ids ) );
+                    update_post_meta($product_id, '_product_image_gallery', implode(',', $gallery_ids));
 
-                    set_post_thumbnail( $product_id, $attach_id );
-
+                    set_post_thumbnail($product_id, $attach_id);
                 }
             }
 
@@ -194,4 +203,4 @@ function product_insert_woocommerce() {
         }
     }
 }
-add_shortcode( 'insert_product_api', 'product_insert_woocommerce' );
+add_shortcode('insert_product_api', 'product_insert_woocommerce');
