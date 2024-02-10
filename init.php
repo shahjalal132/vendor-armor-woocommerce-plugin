6087815799cc96c976cc50062e6ee2318a789c22
Shah jalal <rjshahjalal132@gmail.com>
2024-02-10 10:45:21 +0600

    modified init file code formate

--- init.php
+++ init.php
@@ -14,61 +14,61 @@
  * Update URI:        https://example.com/my-plugin/
  */
 
-if (!defined('WPINC')) {
+if ( !defined( 'WPINC' ) ) {
     die;
 }
 
 // Define plugin path
-if (!defined('VENDOR_PLUGIN_PATH')) {
-    define('VENDOR_PLUGIN_PATH', untrailingslashit(plugin_dir_path(__FILE__)));
+if ( !defined( 'VENDOR_PLUGIN_PATH' ) ) {
+    define( 'VENDOR_PLUGIN_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 }
 
 // Define plugin uri
-if (!defined('VENDOR_PLUGIN_URI')) {
-    define('VENDOR_PLUGIN_URI', untrailingslashit(plugin_dir_url(__FILE__)));
+if ( !defined( 'VENDOR_PLUGIN_URI' ) ) {
+    define( 'VENDOR_PLUGIN_URI', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
 }
 
 // Create wp_sync_products db table when plugin activate
-register_activation_hook(__FILE__, 'vendor_amrod_bd_table_create');
+register_activation_hook( __FILE__, 'vendor_amrod_bd_table_create' );
 
 //deactivation hook
-register_deactivation_hook(__FILE__, 'vendor_amrod_bd_table_remove');
+register_deactivation_hook( __FILE__, 'vendor_amrod_bd_table_remove' );
 
 // Create wp_sync_products db table when plugin activate
-register_activation_hook(__FILE__, 'vendor_stock_bd_table_create');
+register_activation_hook( __FILE__, 'vendor_stock_bd_table_create' );
 
 //deactivation hook
-register_deactivation_hook(__FILE__, 'vendor_stock_bd_table_remove');
+register_deactivation_hook( __FILE__, 'vendor_stock_bd_table_remove' );
 
 //category table
-register_activation_hook(__FILE__, 'sync_categories_table_creation');
+register_activation_hook( __FILE__, 'sync_categories_table_creation' );
 
 //category table
-register_deactivation_hook(__FILE__, 'sync_categories_table_deletion');
+register_deactivation_hook( __FILE__, 'sync_categories_table_deletion' );
 
 //brand table
-register_activation_hook(__FILE__, 'sync_brand_table_creation');
+register_activation_hook( __FILE__, 'sync_brand_table_creation' );
 
 //brand table
-register_deactivation_hook(__FILE__, 'sync_brand_table_deletion');
+register_deactivation_hook( __FILE__, 'sync_brand_table_deletion' );
 
 //price table
-register_activation_hook(__FILE__, 'sync_price_table_creation');
+register_activation_hook( __FILE__, 'sync_price_table_creation' );
 
 //price table
-register_deactivation_hook(__FILE__, 'sync_price_table_deletion');
+register_deactivation_hook( __FILE__, 'sync_price_table_deletion' );
 
 //sync branding department table
-register_activation_hook(__FILE__, 'sync_branding_departments_table_create');
+register_activation_hook( __FILE__, 'sync_branding_departments_table_create' );
 
 //sync branding department table
-register_deactivation_hook(__FILE__, 'sync_branding_departments_table_remove');
+register_deactivation_hook( __FILE__, 'sync_branding_departments_table_remove' );
 
 //sync branding price table
-register_activation_hook(__FILE__, 'sync_branding_price_table_create');
+register_activation_hook( __FILE__, 'sync_branding_price_table_create' );
 
 //sync branding price table
-register_deactivation_hook(__FILE__, 'sync_branding_price_table_remove');
+register_deactivation_hook( __FILE__, 'sync_branding_price_table_remove' );
 
 
 
